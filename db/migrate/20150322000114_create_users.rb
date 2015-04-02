261244c4f03e70082a8b50f4f1784339f0c9fa6d
Aurali <dryeena3@gmail.com>
2015-04-01 21:46:15 -0400

    migrate fix

--- db/migrate/20150322000114_create_users.rb
+++ db/migrate/20150322000114_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :passwod_digest
+      t.string :password_digest
       t.string :email
       t.integer :level, default: 0, value: 0
       t.inet :ip_Address
