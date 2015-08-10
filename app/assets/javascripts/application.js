0757f12f43dc8a3cd8cc6cc2b4e417c608a8cd11
TwilightStormshi <twilightstormshi@gmail.com>
2015-08-10 14:28:08 -0700

    banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,89 +16,17 @@
 //= require bootstrap-sprockets
 //= require prefixfree-rails/prefixfree
 
-// function ready(){
-// 	header = $("#top-nav");
-// 	main = $("main");
-// 	adjustForHeader();
-// 	adjustForScrollBar();
+$(window).load(function(){
+  var navbar = $("header").children(".navbar"),
+      separator = $(".separator");
 
-// 	ex = function(obj){
-// 		return typeof obj !== "undefined";
-// 	};
+  navbar.affix({
+    offset: {
+      top: 0
+    }
+  });
 
-// 	detachPoint = $("#detach-point");
-
-// 	make = function me(hash, attachPoint){
-// 		if(!ex(attachPoint)) return;
-// 		$.each(hash, function(key, value){
-// 			tag = ex(value.tag)? "<"+value.tag+" />" : "<div />";
-// 			klass = ex(value.klass)? value.klass : "";
-// 			text = ex(value.text)? value.text : "";
-// 			css = ex(value.css)? value.css : {};
-// 			children = ex(hash[key].children)? hash[key].children : {}
-// 			element = $("#"+key);
-// 			element = ex(element[0])? element :
-// 				$(tag, {
-// 					class: klass,
-// 					id: key,
-// 					text: text
-// 				});
-// 			console.log(key, value.before);
-
-// 			if(ex(value.before)) element.insertBefore($(value.before));
-// 			else element.appendTo(attachPoint);
-
-// 			me(children,
-// 				element
-// 				.css("display", "flex")
-// 				.css(css)
-// 			);
-			
-			
-// 		});
-// 	};
-// 	if(typeof layout !== 'undefined'){
-// 	make(layout, $("#attach-point"));
-// 	detachPoint.remove();
-
-// 	};
-
-// 	// attachPoint = $("#attach-point");
-// 	// if(typeof attachPoint !== "undefined"){
-// 	// 	attachPoint
-// 	// 	.append($("#detach").css({
-// 	// 		"display": "flex",
-// 	// 		"justify-content": "space-around",
-// 	// 		"margin": "2% 0"
-// 	// 	}))
-// 	// 	.append($("#save-button"));
-// 	// 	$("#detach-point").detach();
-// 	// }
-// }
-
-// $(document).ready(ready);
-// $(document).on('page:load', ready);
-
-// $(window).resize(function(){
-// 	adjustForHeader();
-// 	adjustForScrollBar();
-// });
-
-// function adjustForHeader(){
-// 	height = header.css("height");
-// 	main.css("margin-top", height);
-// }
-
-// function adjustForScrollBar(){
-// 	if($("body").hasScrollBar()){
-// 		$(".navbar-right").css("margin-right", "2px");
-// 	}else{
-// 		$(".navbar-right").css("margin-right", "0");
-// 	}
-// }
-
-// (function($) {
-//     $.fn.hasScrollBar = function() {
-//         return $(document).height() > $(window).height();
-//     }
-// })(jQuery);
+  $(window).scroll(function(){
+    
+  })
+});
