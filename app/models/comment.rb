32bda793ea3ffb5e273c9ae28527b6a1bf87b477
TwilightStormshi <twilightstormshi@gmail.com>
2015-09-04 09:45:05 -0700

    partial message system, some css polish

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
   has_many :comments
+
+  validates :content, format: { with: /\S+/, message: "must have some content." }
 end
