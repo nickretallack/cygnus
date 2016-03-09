f4295c5451c3924108b0993a0d98acdeb8b720ae
Twilight Stormshi <twilightstormshi@gmail.com>
2016-03-09 14:40:00 -0500

    bodii

--- app/helpers/submission_helper.rb
+++ app/helpers/submission_helper.rb
@@ -1,7 +1,7 @@
 module SubmissionHelper
 
   def recent_submissions
-    Submission.where("(select array_to_string(attachments, ',') from submissions) ~ 'image'").where(hidden: false).order("id desc").limit(20).reverse
+    Submission.where("array_to_string(attachments, ',') ~ 'image'").where(hidden: false).order("id desc").limit(20).reverse
   end
 
 end
