29ec64af08f472abf0eb42cb68ffc282279e7278
Twilight Stormshi <twilightstormshi@gmail.com>
2016-03-26 21:29:00 -0400

    markdown on announcements and no more sensitive migration of a certain admin varietal

--- db/migrate/20160324190858_set_admins.rb
+++
@@ -1,6 +0,0 @@
-class SetAdmins < ActiveRecord::Migration
-  def up
-    User.find("Aurali").update_attribute(:level, "admin")
-    User.find("TwilightStormshi").update_attribute(:level, "admin")
-  end
-end
